8cd86ff29d20dcaabe163c346aff602d005b1de8
Jeremy Hajek <hajek@iit.edu>
2016-12-19 14:15:23 -0600

    Updated the riemann configurations

--- packer/scripts/post_install_riemanna_setup.sh
+++ packer/scripts/post_install_riemanna_setup.sh
@@ -12,4 +12,12 @@ dpkg -i riemann_0.2.11_all.deb
 
 sudo service riemann start
 
+# P. 44  Install ruby gem tools
+sudo gem install --no-ri --no-rdoc riemann-tools
+
+
+# http://stackoverflow.com/questions/5171901/sed-command-find-and-replace-in-file-and-overwrite-file-doesnt-work-it-empties
+
+sudo sed -i 's/ubuntu/riemanna/g' /etc/hostname; sudo service hostname restart; exec bash
+
 
