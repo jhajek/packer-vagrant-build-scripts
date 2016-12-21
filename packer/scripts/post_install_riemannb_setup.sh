a80b881d6829d7a9537e7624a5cd617792f04e00
jeremy Hajek <hajek@iit.edu>
2016-12-21 16:08:39 -0600

    Added code to install vagrant private key to post install script

--- packer/scripts/post_install_riemannb_setup.sh
+++ packer/scripts/post_install_riemannb_setup.sh
@@ -20,4 +20,11 @@ sudo gem install --no-ri --no-rdoc riemann-tools
 
 sudo sed -i 's/ubuntu/riemannb/g' /etc/hostname; sudo service hostname restart; exec bash
 
+# Installing vagrant keys 
+mkdir ~/.ssh 
+chmod 700 ~/.ssh 
+cd ~/.ssh 
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys 
+chmod 600 ~/.ssh/authorized_keys 
+chown -R vagrant ~/.ssh
 
