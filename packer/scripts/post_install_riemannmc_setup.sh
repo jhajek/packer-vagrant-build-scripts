183847e59593052fb0bd6f613d77ff549363418f
jeremy Hajek <hajek@iit.edu>
2016-12-23 11:27:45 -0600

    Changed sed -i command to fix typo

--- packer/scripts/post_install_riemannmc_setup.sh
+++ packer/scripts/post_install_riemannmc_setup.sh
@@ -18,7 +18,7 @@ sudo gem install --no-ri --no-rdoc riemann-tools
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 #sudo usermod -a -G sudo vagrant
 sudo mkdir -p /etc/sudoers.d/90-cloud-init-users
-sudo sed -i "ubuntu ALL=(ALL) NOPASSWD:ALL" /etc/sudoers.d/90-cloud-init-users
+sudo sed -i "\$aubuntu ALL=(ALL) NOPASSWD:ALL" /etc/sudoers.d/90-cloud-init-users
 
 
 
