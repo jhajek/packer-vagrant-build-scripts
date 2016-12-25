accea517d016e6ac80debc1508db08959325fce4
jeremy Hajek <hajek@iit.edu>
2016-12-25 17:42:27 -0600

    Updated the URL to add the current repo ~2.3

--- packer/scripts/post_install_jenkins.sh
+++ packer/scripts/post_install_jenkins.sh
@@ -4,17 +4,12 @@ set -v
 
 sudo timedatectl set-timezone America/Chicago
 
-
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-
-echo "deb http://pkg.jenkins-ci.org/debian binary/" | sudo tee -a /etc/apt/sources.list
-
 sudo echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/vagrant
 sudo echo 'Defaults:vagrant !requiretty' >> /etc/sudoers.d/vagrant
 
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee -a /etc/apt/sources.list
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt-get install -y jenkins
 sudo apt-get install -y dkms linux-headers-$(uname -r)
-
