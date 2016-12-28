9e35a6a597ddfc6ebfdeb777e64e0a8138544463
jeremy Hajek <hajek@iit.edu>
2016-12-27 21:27:37 -0600

    Updated two files to properly install pre-reqs

--- packer/scripts/post_install_riemannb_setup-centos-7.sh
+++ packer/scripts/post_install_riemannb_setup-centos-7.sh
@@ -2,23 +2,35 @@
 set -e
 set -v
 
-
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
 # This line assumes the user you created in the preseed directory is vagrant
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
 sudo cat /etc/sudoers.d/init-users
 
-sudo yum install -y wget git gcc
-
 # Install Elrepo - The Community Enterprise Linux Repository (ELRepo) - http://elrepo.org/tiki/tiki-index.php
 sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+sudo yum install -y epel-release # https://wiki.centos.org/AdditionalResources/Repositories
+
+# Install base dependencies -  Centos 7 mininal needs the EPEL repo in the line above and the package daemonize
+sudo yum update -y
+sudo yum install -y wget git gcc java-1.7.0-openjdk daemonize
 
 # Installing vagrant keys
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 sudo mkdir -p /home/vagrant/.ssh
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+
+# Fetch the riemann RPM
+wget https://aphyr.com/riemann/riemann-0.2.11-1.noarch.rpm
+sudo rpm -Uvh riemann-0.2.11-1.noarch.rpm
+
+sudo systemctl start riemann
+
+# P. 44  Install ruby gem tool, Centos 7 has Ruby 2.x as the default
+sudo yum install -y ruby ruby-devel gcc libxml2-devel
+sudo gem install --no-ri --no-rdoc riemann-tools
 
 echo "All Done!"
