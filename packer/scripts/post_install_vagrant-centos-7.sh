8151932639b9e26833efe1c6eeec2a332688ddb0
jeremy Hajek <hajek@iit.edu>
2016-12-27 20:36:35 -0600

    Updated the base.cfg added new post_install file

---
+++ packer/scripts/post_install_vagrant-centos-7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+set -e
+set -v
+
+
+# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+# This line assumes the user you created in the preseed directory is vagrant
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
+sudo cat /etc/sudoers.d/init-users
+
+sudo yum install -y wget git gcc
+
+# Installing vagrant keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+sudo mkdir -p /home/vagrant/.ssh
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+
+echo "All Done!"
