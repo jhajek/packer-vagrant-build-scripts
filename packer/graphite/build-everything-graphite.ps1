1ac6ea45da4ff6998e14519c353c7cfaecd3529d
jeremy Hajek <hajek@iit.edu>
2017-01-01 02:06:08 -0600

    Created the Graphite template json files

---
+++ packer/graphite/build-everything-graphite.ps1
@@ -0,0 +1,1 @@
+packer build ./centos-7-1611-riemanna.json ; packer build ./centos-7-1611-riemannb.json ; packer build ./centos-7-1611-riemannmc.json ; packer build ./ubuntu14045-riemanna.json ; packer build ./ubuntu14045-riemannb.json ; packer build ./ubuntu14045-riemannmc.json
