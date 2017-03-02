2c69108ec57984e644ca0850cd7c5f4e9b294444
Jeremy Hajek <hajek@iit.edu>
2017-03-01 22:39:24 -0600

    Rearranged order

--- packer/scripts/post_install_graphitea_setup.sh
+++ packer/scripts/post_install_graphitea_setup.sh
@@ -2,6 +2,19 @@
 set -e
 set -v
 
+
+# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
+sudo cat /etc/sudoers.d/init-users
+
+# Installing vagrant keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+sudo mkdir -p /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+
+
 sudo apt-get update -y
 #http://askubuntu.com/questions/549550/installing-graphite-carbon-via-apt-unattended
 sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y --force-yes install graphite-carbon
@@ -27,13 +40,3 @@ curl https://packagecloud.io/gpg.key | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install -y grafana
 
-# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
-# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
-echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
-sudo cat /etc/sudoers.d/init-users
-
-# Installing vagrant keys
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-sudo mkdir -p /home/vagrant/.ssh
-cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
-sudo chown -R vagrant:vagrant /home/vagrant/.ssh
