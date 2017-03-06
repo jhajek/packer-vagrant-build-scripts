0528194b56c3689805e162fb14a4782580b6bc09
Jeremy Hajek <hajek@iit.edu>
2017-03-06 13:15:32 -0600

    Updated post_install_graphitemc

--- packer/scripts/post_install_graphitemc_setup.sh
+++ packer/scripts/post_install_graphitemc_setup.sh
@@ -1,31 +1,42 @@
 #!/bin/bash 
 set -e
 set -v
-# http://stackoverflow.com/questions/26595620/how-to-install-ruby-2-1-4-on-ubuntu-14-04
-sudo apt-add-repository -y ppa:brightbox/ruby-ng
-sudo apt-get update -y
-sudo apt-get install -y ruby2.2 ruby2.2-dev build-essential zlib1g-dev openjdk-7-jre
 
-
-# P.42 The Art of Monitoring
-wget http://aphyr.com/riemann/riemann_0.2.11_all.deb
-dpkg -i riemann_0.2.11_all.deb
-
-sudo service riemann start
-
-# P. 44  Install ruby gem tools
-sudo gem install --no-ri --no-rdoc riemann-tools
 
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
 sudo cat /etc/sudoers.d/init-users
 
-
-# Installing vagrant keys 
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'  
+# Installing vagrant keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 sudo mkdir -p /home/vagrant/.ssh
 cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 
+
+sudo apt-get update -y
+#http://askubuntu.com/questions/549550/installing-graphite-carbon-via-apt-unattended
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y --force-yes install graphite-carbon
+sudo apt-get install -y apt-transport-https 
+
+# P.134 - Listing 4.10: Adding the Graphite-API Package Cloudkey
+curl https://packagecloud.io/gpg.key | sudo apt-key add -
+
+# P.134 - Listing 4.11: Adding the Package Cloud exoscale repository listing
+sudo sh -c "echo deb https://packagecloud.io/exoscale/community/ubuntu/ trusty main > /etc/apt/sources.list.d/exoscale_community.list"
+sudo apt-get update -y 
+
+# P.135 - Listing 4.13: Installing the graphite-api package on Ubuntu
+sudo apt-get install -y graphite-api
+
+# P.136 - Listing 4.16: Adding the Grafana repository listing
+sudo sh -c "echo deb https://packagecloud.io/grafana/stable/debian/ wheezy main > /etc/apt/sources.list.d/packagecloud_grafana.list"
+
+# P.137 - Listing 4.17: Adding the Package Cloudkey
+curl https://packagecloud.io/gpg.key | sudo apt-key add -
+
+# P.137 - Listing 4.18: Installing the Grafana package
+sudo apt-get update -y
+sudo apt-get install -y grafana
 
