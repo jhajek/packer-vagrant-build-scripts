2bbfc74613bdd8cbfe31454a4e444dcda20d9178
jeremy Hajek <hajek@iit.edu>
2017-03-13 11:31:34 -0500

    Changed the post install script for jenkins

--- packer/scripts/post_install_jenkins.sh
+++ packer/scripts/post_install_jenkins.sh
@@ -4,12 +4,19 @@ set -v
 
 sudo timedatectl set-timezone America/Chicago
 
-sudo echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/vagrant
-sudo echo 'Defaults:vagrant !requiretty' >> /etc/sudoers.d/vagrant
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee -a /etc/apt/sources.list
+# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
+sudo cat /etc/sudoers.d/init-users
+
+# Installing vagrant keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+sudo mkdir -p /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
 sudo apt-get update -y
 sudo apt-get install -y openjdk-7-jre
-sudo apt-get install -y jenkins
