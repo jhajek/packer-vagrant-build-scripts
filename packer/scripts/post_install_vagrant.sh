e3bc0be53a59a82d3cc319aa1af63c6a27e779e8
jeremy Hajek <hajek@iit.edu>
2017-03-25 10:04:31 -0500

    Updated ubuntu post install

--- packer/scripts/post_install_vagrant.sh
+++ packer/scripts/post_install_vagrant.sh
@@ -6,8 +6,9 @@ set -v
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
 # This line assumes the user you created in the preseed directory is vagrant
-echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
-sudo cat /etc/sudoers.d/init-users
+echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
+sudo groupadd admin
+sudo usermod -a -G admin vagrant
 
 
 # Installing vagrant keys
