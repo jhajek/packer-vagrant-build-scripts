4b4e8a4b28fe7aa1eb41f5371dfe053acd65be63
Jeremy Hajek <hajek@iit.edu>
2017-07-27 15:12:26 -0500

    Updated the ~.my.cnf

--- packer/scripts/post_install_moodle_32.sh
+++ packer/scripts/post_install_moodle_32.sh
@@ -67,6 +67,6 @@ ufw allow proto tcp to 0.0.0.0/0 port 80
 # Inject the username and password for autologin later in a ~/.my.cnf file
 # http://serverfault.com/questions/103412/how-to-change-my-mysql-root-password-back-to-empty/103423#103423
 
-echo -e "[client] \n user = root \n password = $MARIADBPASSWORD" > ~/.my.cnf
+echo -e "[client] \n user = root \n password = PASS" > ~/.my.cnf
 echo -e "\n port = 3306 \n socket          = /var/run/mysqld/mysqld.sock" >> ~/.my.cnf
 
