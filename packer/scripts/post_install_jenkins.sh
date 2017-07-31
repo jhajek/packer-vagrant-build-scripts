274cb9e77b03163610b4ec6e4c26715a7caefac2
Jeremy Hajek <hajek@iit.edu>
2017-07-31 00:24:57 -0500

    Updated Jenkins post-install script to add correct Vagrant keys

--- packer/scripts/post_install_jenkins.sh
+++ packer/scripts/post_install_jenkins.sh
@@ -4,17 +4,21 @@ set -v
 
 sudo timedatectl set-timezone America/Chicago
 
-
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
-echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
-sudo cat /etc/sudoers.d/init-users
+# This line assumes the user you created in the preseed directory is vagrant
+echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
+sudo groupadd admin
+sudo usermod -a -G admin vagrant
+
 
 # Installing vagrant keys
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 sudo mkdir -p /home/vagrant/.ssh
-cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+echo "All Done!"
 
 wget http://mirrors.jenkins.io/war-stable/2.60.1/jenkins.war
 
