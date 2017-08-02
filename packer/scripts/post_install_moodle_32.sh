fe878f129109d6d167405a1f8e83f39c016da251
Jeremy Hajek <hajek@iit.edu>
2017-08-02 01:16:29 -0500

    Added chmod +x for db.sh

--- packer/scripts/post_install_moodle_32.sh
+++ packer/scripts/post_install_moodle_32.sh
@@ -64,6 +64,7 @@ ufw allow proto tcp to 0.0.0.0/0 port 80
 cd ~
 git clone https://github.com/jhajek/commands
 chmod +x ~/commands/cnf/cnf.sh
+chomd +x ~/commands/cnf/db.sh
 ~/commands/cnf/cnf.sh
 # Added the mariadb my.cnf configs for creating Barracuda
 sudo ~/commands/cnf/db.sh
