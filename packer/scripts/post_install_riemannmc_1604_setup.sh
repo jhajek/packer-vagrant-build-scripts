bfb4dcb66515fa00e716c48cfb0e710d9bf60bd8
Jeremy Hajek <hajek@iit.edu>
2017-08-15 23:17:58 -0500

    Changed order of script

--- packer/scripts/post_install_riemannmc_1604_setup.sh
+++ packer/scripts/post_install_riemannmc_1604_setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash 
 set -e
 set -v
+
+# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
+sudo cat /etc/sudoers.d/init-users
+
+# Installing vagrant keys 
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'  
+sudo mkdir -p /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+
 # http://stackoverflow.com/questions/26595620/how-to-install-ruby-2-1-4-on-ubuntu-14-04
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update -y
@@ -16,16 +28,5 @@ sudo service riemann start
 # P. 44  Install ruby gem tools
 sudo gem install --no-ri --no-rdoc riemann-tools
 
-# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
-# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
-echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-users
-sudo cat /etc/sudoers.d/init-users
-
-
-# Installing vagrant keys 
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'  
-sudo mkdir -p /home/vagrant/.ssh
-cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
-sudo chown -R vagrant:vagrant /home/vagrant/.ssh
 
 
