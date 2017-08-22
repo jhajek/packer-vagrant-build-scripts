3302e09ca8ddd30db9fbb44baf9230b223040649
Jeremy Hajek <hajek@iit.edu>
2017-08-22 17:38:26 -0500

    Added Command line variables for the definition of the root and userDB password

--- packer/scripts/post_install_coupdoeil-backend.sh
+++ packer/scripts/post_install_coupdoeil-backend.sh
@@ -19,8 +19,13 @@ cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
 sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 echo "All Done!"
 
-
 # Install instructions https://github.com/illinoistech-itm/coup-doeil
+# https://dba.stackexchange.com/questions/59317/install-mariadb-10-on-ubuntu-without-prompt-and-no-root-password
+# http://dba.stackexchange.com/questions/35866/install-mariadb-without-password-prompt-in-ubuntu?newreg=426e4e37d5a2474795c8b1c911f0fb9f
+# From <http://serverfault.com/questions/103412/how-to-change-my-mysql-root-password-back-to-empty/103423> 
+export DEBIAN_FRONTEND=noninteractive
+echo "mariadb-server mysql-server/root_password password $DBPASS" | sudo  debconf-set-selections
+echo "mariadb-server mysql-server/root_password_again password $DBPASS" | sudo debconf-set-selections
 
 sudo apt-get -y update && sudo apt-get -y dist-upgrade 
 sudo apt-get -y install nodejs npm
