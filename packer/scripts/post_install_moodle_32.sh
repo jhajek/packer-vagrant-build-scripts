3dfeba5e576ca98d41fe0dc80fae0ae69b2cee91
Jeremy Hajek <hajek@iit.edu>
2017-10-18 21:52:19 -0500

    Fixed moodle typo

--- packer/scripts/post_install_moodle_32.sh
+++ packer/scripts/post_install_moodle_32.sh
@@ -94,7 +94,7 @@ cd ~
 git clone https://github.com/jhajek/commands
 chmod +x ~/commands/cnf/cnf.sh
 chmod +x ~/commands/cnf/db.sh
-~/commands/cnf/cnf.sh
+chmod +x ~/commands/cnf/cnf.sh
 # Added the mariadb my.cnf configs for creating Barracuda
 sudo ~/commands/cnf/db.sh
 # Restart mariadb service after adding custom .my.cnf
