c6c8f828addd825d2748d42b2eab086d8d7a06f1
Jeremy Hajek <hajek@iit.edu>
2017-10-19 00:13:54 -0500

    Removed duplicate git clone command

--- packer/scripts/post_install_moodle_32.sh
+++ packer/scripts/post_install_moodle_32.sh
@@ -91,7 +91,6 @@ ufw allow proto tcp to 0.0.0.0/0 port 443
 
 # Mariadb create user and tables commands from https://github.com/jhajek/commands.git
 cd ~
-git clone https://github.com/jhajek/commands
 chmod +x ~/commands/cnf/cnf.sh
 chmod +x ~/commands/cnf/db.sh
 chmod +x ~/commands/cnf/cnf.sh
