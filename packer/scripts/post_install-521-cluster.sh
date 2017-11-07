e67d8577481f974ed790eca8c0c4b5e1d9e5dd8d
Jeremy Hajek <hajek@iit.edu>
2017-11-07 09:19:17 -0600

    created a cluster with the controller username

---
+++ packer/scripts/post_install-521-cluster.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+set -e
+set -v
+
+
+# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+# This line assumes the user you created in the preseed directory is controller
+echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
+sudo groupadd admin
+sudo usermod -a -G admin controller 
+
+
+# Installing controller keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/controller/master/keys/vagrant.pub'
+sudo mkdir -p /home/controller/.ssh
+sudo chown -R controller:controller /home/controller/.ssh
+cat ./vagrant.pub >> /home/controller/.ssh/authorized_keys
+sudo chown -R controller:controller /home/controller/.ssh/authorized_keys
+echo "All Done!"
+
+#http://www.fail2ban.org/wiki/index.php/MANUAL_0_8#Jails
+sudo sed -i "s/bantime = 600/bantime = -1/g" /etc/fail2ban/jail.conf
+sudo systemctl enable fail2ban
+sudo service fail2ban restart
+
+##################################################
+# Add User customizations below here
+##################################################
+
+cat << EOT >> ~/.bashrc 
+
+########## Inserted by Jeremy
+export JAVA_HOME=/usr
+export HADOOP_HOME=/home/controller/hadoop-2.6.5
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/home/controller/hadoop-2.6.5/bin:/home/controller/hadoop-2.6.5/sbin:/usr/local/bin
+export HADOOP_CLASSPATH=/usr/lib/jvm/java-8-oracle/lib/tools.jar
+EOT
+
+# http://askubuntu.com/questions/493460/how-to-install-add-apt-repository-using-the-terminal
+sudo apt-get update ; sudo apt-get install -y software-properties-common 
+sudo add-apt-repository -y ppa:webupd8team/java
+# Oracle JDK needed for version 1.8 as openJDK is not backported for Ubuntu 14.04.5 - http://askubuntu.com/questions/521145/how-to-install-oracle-java-on-ubuntu-14-04
+# It has a license you need to accpet - here is how to auto do that
+# http://askubuntu.com/questions/190582/installing-java-automatically-with-silent-option
+
+sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+
+#http://www.himpfen.com/install-java-ubuntu/
+#http://blog.cloudera.com/blog/2009/11/hadoop-at-twitter-part-1-splittable-lzo-compression/
+sudo apt-get -y update; 
+sudo apt-get install -y oracle-java8-installer
+
+sudo apt-get -y update ; sudo apt-get -y install pkgconf wget liblzo2-dev sysstat iotop vim libssl-dev libsnappy-dev libsnappy-java libbz2-dev libgcrypt11-dev zlib1g-dev lzop htop fail2ban
+
+# Download Hadoop 2.6.5 source and extract tarbal
+wget http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz
+tar -xvzf hadoop-2.6.5.tar.gz
+
