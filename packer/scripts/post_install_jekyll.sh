5d263419a725fc0e5fc1d632b60c78c86de6ceee
Jeremy Hajek <hajek@iit.edu>
2017-11-18 17:20:37 -0600

    Updated forge install script to grab the clean blog theme and its dependencies

--- packer/scripts/post_install_jekyll.sh
+++ packer/scripts/post_install_jekyll.sh
@@ -28,5 +28,8 @@ sudo gem install sass --version 3.4.24
 git clone https://github.com/jhajek/forge.git /home/vagrant/Documents
 sudo chown -R vagrant:vagrant /home/vagrant/Documents
 
-cd forge
-bundle install 
+sudo gem install -y jekyll-theme-clean-blog
+
+cd ~/forge
+bundle update jekyll 
+bundle install
