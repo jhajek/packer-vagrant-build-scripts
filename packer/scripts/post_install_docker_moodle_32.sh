0c8c13a95757077b22b4d0f51249ac62b95d5225
Jeremy Hajek <hajek@iit.edu>
2018-01-02 19:54:24 -0600

    Changed riemann restart to start

--- packer/scripts/post_install_docker_moodle_32.sh
+++ packer/scripts/post_install_docker_moodle_32.sh
@@ -40,7 +40,7 @@ wget https://github.com/riemann/riemann/releases/download/0.2.14/riemann_0.2.14_
 
  systemctl enable collectd
  service collectd start
- systemctl restart riemann
+ systemctl start riemann
 # End of collectd and riemann install
 
  systemctl enable mysql.service
