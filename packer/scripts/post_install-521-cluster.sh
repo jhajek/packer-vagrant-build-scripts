639dd9fcd0340dcd6ea0cac8bde38a38f5ff5fdb
Jeremy Hajek <hajek@iit.edu>
2018-01-23 14:52:45 -0600

    Updated Cluster information and put it back to vagrant -- will figure out cluster permissions after this...

--- packer/scripts/post_install-521-cluster.sh
+++ packer/scripts/post_install-521-cluster.sh
@@ -5,18 +5,18 @@ set -v
 
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
-# This line assumes the user you created in the preseed directory is controller
+# This line assumes the user you created in the preseed directory is vagrant
 echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
 sudo groupadd admin
-sudo usermod -a -G admin controller 
+sudo usermod -a -G admin vagrant
 
 
-# Installing controller keys
+# Installing vagrant keys
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-sudo mkdir -p /home/controller/.ssh
-sudo chown -R controller:controller /home/controller/.ssh
-cat ./vagrant.pub >> /home/controller/.ssh/authorized_keys
-sudo chown -R controller:controller /home/controller/.ssh/authorized_keys
+sudo mkdir -p /home/vagrant/.ssh
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 echo "All Done!"
 
 #http://www.fail2ban.org/wiki/index.php/MANUAL_0_8#Jails
@@ -28,12 +28,12 @@ sudo service fail2ban restart
 # Add User customizations below here
 ##################################################
 
-cat << EOT >> /home/controller/.bashrc 
+cat << EOT >> /home/vagrant/.bashrc 
 
 ########## Inserted by Jeremy
 export JAVA_HOME=/usr
-export HADOOP_HOME=/home/controller/hadoop-2.6.5
-export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/home/controller/hadoop-2.6.5/bin:/home/controller/hadoop-2.6.5/sbin:/usr/local/bin
+export HADOOP_HOME=/home/vagrant/hadoop-2.6.5
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/home/vagrant/hadoop-2.6.5/bin:/home/vagrant/hadoop-2.6.5/sbin:/usr/local/bin
 export HADOOP_CLASSPATH=/usr/lib/jvm/java-8-oracle/lib/tools.jar
 EOT
 
