669c85a68d96132b52ba4e583fa65835d1861c95
Jeremy Hajek <hajek@iit.edu>
2018-01-31 21:50:39 -0600

    add sudo before Git Clone...

--- packer/scripts/post_install_vagrant-class-github.sh
+++ packer/scripts/post_install_vagrant-class-github.sh
@@ -30,15 +30,15 @@ sudo service fail2ban restart
 # Here we are adding the basic contents of a file named: config and place that in .ssh directory so as to relate the private key to the github deploy key
 
 # You need to move private key to the correct directory location - place the location in the root user's home directory because these commands are executed not as the user but as root...
-mv /home/vagrant/id_rsa_github_deploy_key /root/.ssh/
+sudo mv /home/vagrant/id_rsa_github_deploy_key /root/.ssh/
 # You need to move the ssh config file to the correct directory location
-mv /home/vagrant/config /root/.ssh/
+sudo mv /home/vagrant/config /root/.ssh/
 
 # You need to change the permission of the private key 
 chmod 600 /root/.ssh/id_rsa_github_deploy_key
 
 # clone a private repo with the key
 # https://stackoverflow.com/questions/4565700/specify-private-ssh-key-to-use-when-executing-shell-command-with-or-without-ruby
-git clone git@github.com:illinoistech-itm/hajek.git
+sudo git clone git@github.com:illinoistech-itm/hajek.git
 
 
