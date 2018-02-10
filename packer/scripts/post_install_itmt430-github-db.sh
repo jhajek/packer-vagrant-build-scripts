9fd2d9ea277d356fe64718f120e99325dd885ff7
Jeremy Hajek <hajek@iit.edu>
2018-02-09 21:27:26 -0600

    Fixed white spaces

--- packer/scripts/post_install_itmt430-github-db.sh
+++ packer/scripts/post_install_itmt430-github-db.sh
@@ -2,14 +2,12 @@
 set -e
 set -v
 
-
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
 # This line assumes the user you created in the preseed directory is vagrant
 echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
 sudo groupadd admin
 sudo usermod -a -G admin vagrant
-
 
 # Installing vagrant keys
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
@@ -38,7 +36,6 @@ sudo apt-get install -y mariadb-server
 # chown the cloned github repo files so the user owns it 
 sudo chown -R vagrant:vagrant ~/hajek
 # copying the php code to the /var/www/html directory to serve php files
-
 
 # Enable the service and start the service
 sudo systemctl enable mysql
