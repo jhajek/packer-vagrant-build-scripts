887f567dfcfe05b614f894c34d0e454eb6326270
Jeremy Hajek <hajek@iit.edu>
2018-02-12 08:45:59 -0600

    changed relative path to absolute path

--- packer/scripts/post_install_itmt430-github-db.sh
+++ packer/scripts/post_install_itmt430-github-db.sh
@@ -45,11 +45,11 @@ sudo systemctl start mysql
 # http://serverfault.com/questions/103412/how-to-change-my-mysql-root-password-back-to-empty/103423#103423
 # https://stackoverflow.com/questions/8020297/mysql-my-cnf-file-found-option-without-preceding-group
 
-echo -e "[mysqld]" > ~/.my.cnf
+echo -e "[mysqld]" > /home/vagrant/.my.cnf
 echo -e "\n\n[client]\nuser = root\npassword = $DBPASS" >> ~/.my.cnf
 echo -e "\nport = 3306\nsocket = /var/run/mysqld/mysqld.sock\n" >> ~/.my.cnf
 
-echo -e "[mysqld]\n\n" > ~/.my.cnf.backup
+echo -e "[mysqld]\n\n" > /homer/vagrant/.my.cnf.backup
 echo -e "[client]\nuser = worker\npassword = $USERPASS" >> ~/.my.cnf.worker
 echo -e "\nport = 3306\nsocket = /var/run/mysqld/mysqld.sock\n" >> ~/.my.cnf.worker
 echo -e "\ndefault-character-set = utf8mb4\n" >> ~/.my.cnf.worker
