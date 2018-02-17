31695065c41148222732b127fbd2cd3a909e55d1
Jeremy Hajek <hajek@iit.edu>
2018-02-17 17:09:13 -0600

    removed extra w

--- packer/scripts/post_install_itmt430-github-ws.sh
+++ packer/scripts/post_install_itmt430-github-ws.sh
@@ -40,7 +40,7 @@ sudo cp ./hajek/itmt-430/db-samples/*.php /var/www/html
 
 # include statement to place values into an include file for the connection string in test-select.php
 # https://unix.stackexchange.com/questions/4335/how-to-insert-text-into-a-root-owned-file-using-sudo
-echo -e "\$endpoint=$DATABASEIP;  // this is the public IP of the database server" | sudo tee -a /var/wwww/html/connection-info.php
+echo -e "\$endpoint=$DATABASEIP;  // this is the public IP of the database server" | sudo tee -a /var/www/html/connection-info.php
 echo -e "\$user=$DBUSERNAME; //this is the same username that you created in the create-user-with-grants.sql file, change this from root as root is not allowed to make remote connections at all in mysql anymore" | sudo tee -a /var/www/html/connection-info.php
 echo -e "\$password=$USERPASS;  //this is the password that you entered in the create-user-with-grants.sql file after the IDENTIFIED BY string" | sudo tee -a /var/www/html/connection-info.php
 echo -e "\$db-name=$DBNAME; //this is the name of the database you created in create.sql -- store if you keep the default setting" | sudo tee -a /var/www/html/connection-info.php 
