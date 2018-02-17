c6f662ca6593be09ad9fcffa0cee7f263d00974a
Jeremy Hajek <hajek@iit.edu>
2018-02-17 17:08:37 -0600

    Updated variables, template and ws dynamic values

--- packer/scripts/post_install_itmt430-github-ws.sh
+++ packer/scripts/post_install_itmt430-github-ws.sh
@@ -24,6 +24,11 @@ sudo sed -i "s/bantime = 600/bantime = -1/g" /etc/fail2ban/jail.conf
 sudo systemctl enable fail2ban
 sudo service fail2ban restart
 
+# enable and allow ports in firewall
+sudo ufw enable
+ufw allow proto tcp to 0.0.0.0/0 port 80
+ufw allow proto tcp to 0.0.0.0/0 port 443
+
 #Install apache2 webserver
 sudo apt-get update
 sudo apt-get install -y apache2 mysql-client php7.0 
@@ -32,6 +37,13 @@ sudo apt-get install -y apache2 mysql-client php7.0
 sudo chown -R vagrant:vagrant ~/hajek
 # copying the php code to the /var/www/html directory to serve php files
 sudo cp ./hajek/itmt-430/db-samples/*.php /var/www/html
+
+# include statement to place values into an include file for the connection string in test-select.php
+# https://unix.stackexchange.com/questions/4335/how-to-insert-text-into-a-root-owned-file-using-sudo
+echo -e "\$endpoint=$DATABASEIP;  // this is the public IP of the database server" | sudo tee -a /var/wwww/html/connection-info.php
+echo -e "\$user=$DBUSERNAME; //this is the same username that you created in the create-user-with-grants.sql file, change this from root as root is not allowed to make remote connections at all in mysql anymore" | sudo tee -a /var/www/html/connection-info.php
+echo -e "\$password=$USERPASS;  //this is the password that you entered in the create-user-with-grants.sql file after the IDENTIFIED BY string" | sudo tee -a /var/www/html/connection-info.php
+echo -e "\$db-name=$DBNAME; //this is the name of the database you created in create.sql -- store if you keep the default setting" | sudo tee -a /var/www/html/connection-info.php 
 
 # Enable the service and start the service
 sudo systemctl enable apache2
