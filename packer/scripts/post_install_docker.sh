bc69899a5708b8b2121b0b0c9665ce283fe22f05
Jeremy R Hajek <hajek@iit.edu>
2019-01-06 22:50:51 -0600

    updated docker.io to docker-ce

--- packer/scripts/post_install_docker.sh
+++ packer/scripts/post_install_docker.sh
@@ -33,9 +33,9 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker-ce
 sudo systemctl enable docker
 
 # install packer so you can buiild docker containers
