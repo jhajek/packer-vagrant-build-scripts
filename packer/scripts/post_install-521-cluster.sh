76da15036e468c8dca900723304d57d97fb726f1
Jeremy R Hajek <hajek@iit.edu>
2019-01-09 22:01:24 -0600

    Added premade hosts

--- packer/scripts/post_install-521-cluster.sh
+++ packer/scripts/post_install-521-cluster.sh
@@ -46,3 +46,19 @@ sudo apt-get -y install pkgconf wget liblzo2-dev sysstat iotop vim libssl-dev li
 wget http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-2.8.5/hadoop-2.8.5.tar.gz
 tar -xvzf ~/hadoop-2.8.5.tar.gz
 
+cat << EOT >> /etc/hosts
+
+# Hadoop Datanodes
+192.168.1.101 datanode1 datanode1.sat.iit.edu
+192.168.1.102 datanode2 datanode2.sat.iit.edu
+192.168.1.103 datanode3 datanode3.sat.iit.edu
+192.168.1.104 datanode4 datanode4.sat.iit.edu
+192.168.1.105 datanode5 datanode5.sat.iit.edu
+#192.168.1.106 datanode6 datanode6.sat.iit.edu
+192.168.1.106 mariadbserver mariadbserver.sat.iit.edu
+
+# namenode
+192.168.1.100 namenode namenode.sat.iit.edu
+#riemannmc
+10.101.0.2 riemann riemann.sat.iit.edu
+EOT
