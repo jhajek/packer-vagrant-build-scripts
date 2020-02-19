4fa9528a5501943f37cec8bac9a5504ab655167b
Jeremy R Hajek <hajek@iit.edu>
2020-02-19 07:59:16 -0600

    Added a note

--- packer/scripts/post_install_itmt430-github-js-app.sh
+++ packer/scripts/post_install_itmt430-github-js-app.sh
@@ -26,8 +26,12 @@ sudo sed -i "s/bantime = 600/bantime = -1/g" /etc/fail2ban/jail.conf
 sudo systemctl enable fail2ban
 sudo service fail2ban restart
 
+################################################################################################################
+# Keep the above lines to make sure that Vagrant will work seamlessly                                          #
+################################################################################################################
 # set the /etc/hosts file to match hostname
 echo "$WEBSERVERIP     ws  ws.sat.iit.edu" | sudo tee -a /etc/hosts
+sudo hostnamectl set-hostname ws
 
 # enable and allow ports in firewall
 # https://serverfault.com/questions/790143/ufw-enable-requires-y-prompt-how-to-automate-with-bash-script
