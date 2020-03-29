73c86550ed28ac48e9adc1d0298412eb71d41892
Jeremy R Hajek <hajek@iit.edu>
2020-03-28 22:25:35 -0500

    updated build-everything

--- packer/vanilla-install/build-everything-vanilla.sh
+++ packer/vanilla-install/build-everything-vanilla.sh
@@ -1,1 +1,1 @@
-packer build ./centos-7-vanilla.json ; packer build ./ubuntu14045-vanilla.json ; packer build ./ubuntu16041-vanilla.json 
+packer build ./centos-7-vanilla.json;  packer build ./ubuntu18044-vanilla.json
