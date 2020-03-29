2873ed60c91de8c1c5cadcbdde8f88a558ab10db
Jeremy R Hajek <hajek@iit.edu>
2020-03-29 13:24:25 -0500

    added vagrant add command

---
+++ packer/build/fullstack/add-boxes.ps1
@@ -0,0 +1,9 @@
+cd ../
+vagrant box add ubuntu-lb-multi-virtualbox-*.box --name lb
+vagrant box add ubuntu-ws1-multi-virtualbox-*.box --name ws1
+vagrant box add ubuntu-ws2-multi-virtualbox-*.box --name ws2
+vagrant box add ubuntu-ws3-multi-virtualbox-*.box --name ws3
+vagrant box add ubuntu-red-multi-virtualbox-*.box --name red
+vagrant box add ubuntu-mm-multi-virtualbox-*.box --name mm
+vagrant box add ubuntu-ms1-multi-virtualbox-*.box --name ms1
+vagrant box add ubuntu-ms2-multi-virtualbox-*.box --name ms2
