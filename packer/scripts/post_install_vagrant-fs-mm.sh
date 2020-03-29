ea2d1c1754225c8d2dbe26ac0ab0e083f29381c0
Jeremy R Hajek <hajek@iit.edu>
2020-03-29 00:56:35 -0500

    fixed incorrect path in mysql log

--- packer/scripts/post_install_vagrant-fs-mm.sh
+++ packer/scripts/post_install_vagrant-fs-mm.sh
@@ -65,7 +65,7 @@ echo -e "\ndefault-character-set = utf8mb4\n" >> /home/vagrant/.my.cnf.user
 # sudo sed -i "s/.*bind-address.*/#bind-address = $DATABASEIP/" /etc/mysql/mysql.conf.d/mysql.cnf
 sudo sed -i "s/.*bind-address.*/#bind-address = $MMIP/" /etc/mysql/mariadb.conf.d/50-server.cnf 
 sudo sed -i "s/.*server-id.*/server-id      =1/" /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo sed -i "s/.*log_bin.*/log_bin = \/var\/log\/mysql\/mysql-bin.log\/log_bin/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i "s/.*log_bin.*/log_bin = \/var\/log\/mysql\/mysql-bin.log/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 
 # Enable the service and start the service
