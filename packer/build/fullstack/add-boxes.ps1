e403de15e949eab93b26c619b0a4ee325e5ffb2b
Jeremy R Hajek <hajek@iit.edu>
2020-03-30 20:19:37 -0500

    commented out non-essential

--- packer/build/fullstack/add-boxes.ps1
+++ packer/build/fullstack/add-boxes.ps1
@@ -3,7 +3,7 @@ vagrant box add ubuntu-lb-multi-virtualbox-*.box --name lb
 vagrant box add ubuntu-ws1-multi-virtualbox-*.box --name ws1
 vagrant box add ubuntu-ws2-multi-virtualbox-*.box --name ws2
 vagrant box add ubuntu-ws3-multi-virtualbox-*.box --name ws3
-vagrant box add ubuntu-red-multi-virtualbox-*.box --name red
-vagrant box add ubuntu-mm-multi-virtualbox-*.box --name mm
-vagrant box add ubuntu-ms1-multi-virtualbox-*.box --name ms1
-vagrant box add ubuntu-ms2-multi-virtualbox-*.box --name ms2
+#vagrant box add ubuntu-red-multi-virtualbox-*.box --name red
+#vagrant box add ubuntu-mm-multi-virtualbox-*.box --name mm
+#vagrant box add ubuntu-ms1-multi-virtualbox-*.box --name ms1
+#vagrant box add ubuntu-ms2-multi-virtualbox-*.box --name ms2
