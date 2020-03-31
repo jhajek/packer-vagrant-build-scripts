060462e32d6c2d4b87074e52d738a6116e474bd4
Jeremy R Hajek <hajek@iit.edu>
2020-03-30 21:19:00 -0500

    added force command to not prompt

--- packer/build/fullstack/remove-all.ps1
+++ packer/build/fullstack/remove-all.ps1
@@ -1,9 +1,10 @@
-vagrant box remove fs-lb
-vagrant box remove fs-ws1
-vagrant box remove fs-ws2
-vagrant box remove fs-ws3
+vagrant box remove --force fs-lb
+vagrant box remove --force fs-ws1
+vagrant box remove --force fs-ws2
+vagrant box remove --force fs-ws3
 <#vagrant box remove fs-mm
 vagrant box remove fs-ms1
 vagrant box remove fs-ms2
 vagrant box remove fs-red
 #>
+cd ../
