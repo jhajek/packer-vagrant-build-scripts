14433ffe6926f62abffdfd554d6da82aa5280c60
Jeremy R Hajek <hajek@iit.edu>
2020-08-11 23:41:45 -0500

    updated adding delete and remove

---
+++ packer/build/itmo-453-553/destroy-and-remove.ps1
@@ -0,0 +1,28 @@
+# Destroy existing vagrant boxes
+Set-Location ub-riemanna
+vagrant destroy -f
+Remove-Item ./.vagrant -Recurse
+Set-Location ../centos-riemannb
+vagrant destroy -f
+Remove-Item ./.vagrant -Recurse
+Set-Location ../ub-riemannmc
+vagrant destroy -f
+Remove-Item ./.vagrant -Recurse
+Set-Location ../ub-graphitea
+vagrant destroy -f
+Remove-Item ./.vagrant -Recurse
+Set-Location ../centos-graphiteb
+vagrant destroy -f
+Remove-Item ./.vagrant -Recurse
+Set-Location ../ub-graphitemc
+vagrant destroy -f
+Remove-Item ./.vagrant -Recurse
+Set-Location ../
+
+# Remove existing vagrant boxes
+vagrant box remove ub-riemanna --force 
+vagrant box remove centos-riemannb --force
+vagrant box remove ub-riemannmc --force 
+vagrant box remove ub-graphitea --force 
+vagrant box remove centos-graphiteb --force 
+vagrant box remove ub-graphitemc --force 
