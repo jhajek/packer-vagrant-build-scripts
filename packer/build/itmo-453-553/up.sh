fbda0cfdaffd4b4ab9cdd600e3686168d55e4e33
Jeremy R Hajek <hajek@iit.edu>
2020-09-30 11:07:02 -0500

    launch graphite first

--- packer/build/itmo-453-553/up.sh
+++ packer/build/itmo-453-553/up.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-cd ub-riemanna
-vagrant up
-cd ../centos-riemannb
-vagrant up
-cd ../ub-riemannmc
-vagrant up
-cd ../ub-graphitea
+
+cd ub-graphitea
 vagrant up
 cd ../centos-graphiteb
 vagrant up
 cd ../ub-graphitemc
+vagrant up
+cd ../ub-riemanna
+vagrant up
+cd ../centos-riemannb
+vagrant up
+cd ../ub-riemannmc
 vagrant up
 cd ../host1
 vagrant up
