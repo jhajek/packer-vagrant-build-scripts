a56684dbb1f217082ea70ada2fa05283a6e90671
Jeremy R Hajek <hajek@iit.edu>
2020-10-04 20:35:22 -0500

    updated samples code

--- packer/scripts/post_install_vagrant-EC2.sh
+++ packer/scripts/post_install_vagrant-EC2.sh
@@ -4,7 +4,7 @@ set -v
 
 # http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
 # http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
-# This line assumes the user you created in the preseed directory is vagrant
+# This line assumes the user you created in the preseed directory is named:  ubuntu
 echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
 sudo groupadd admin
 sudo usermod -a -G admin ubuntu
@@ -12,6 +12,7 @@ sudo usermod -a -G admin ubuntu
 ##################################################
 # Add User customizations below here
 ##################################################
+# needed to disable password authentication via SSH
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 
-git clone git@github.com:illinoistech-itm/hajek.git
-echo "Repo Cloned!"
+sudo apt-get install -y git rsync wget links
