b04e554dc783ee6e136c80462b6212dd2d4ec258
Jeremy R Hajek <hajek@iit.edu>
2020-12-27 23:11:09 -0600

    Added TLS connection false

--- terraform/proxmox/test-setup/prmx.tf
+++ terraform/proxmox/test-setup/prmx.tf
@@ -9,6 +9,7 @@ provider "proxmox" {
     pm_api_url = "https://172.16.1.62:8006/api2/json"
     pm_user = "root@pam"
     pm_password = "cluster"
+    pm_tls_insecure = true
 }
 
 resource "proxmox_vm_qemu" "resource-name" {
