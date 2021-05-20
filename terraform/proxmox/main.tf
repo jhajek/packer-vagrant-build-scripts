41f414dd96441969c72f8747ec29f0706e8dd1f0
Jeremy R Hajek <hajek@iit.edu>
2021-05-19 21:33:25 -0500

    updated private key

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,6 +1,6 @@
-resource "proxmox_vm_qemu" "terraform-k8s" {
+resource "proxmox_vm_qemu" "test" {
   count       = 3
-  name        = "itmt495-k8s-${terraform.workspace}-vm${count.index}"
+  name        = "test-vm${count.index}"
   desc        = var.desc
   target_node = var.target_node
   clone       = var.template_to_clone
@@ -27,13 +27,13 @@ resource "proxmox_vm_qemu" "terraform-k8s" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo hostnamectl set-hostname itmt495-k8s-${terraform.workspace}-vm${count.index}"
+      "sudo hostnamectl set-hostname test-vm${count.index}"
     ]
 
     connection {
       type        = "ssh"
       user        = "vagrant"
-      private_key = file("${path.module}/id_rsa_itmo-453-553-class-server")
+      private_key = file("${path.module}/id_rsa_jrh_sample")
       host        = self.ssh_host
       port        = self.ssh_port
     }
