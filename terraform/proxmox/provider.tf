24024f1b4b9368b9ab0d03adff097cdd6fc065d7
Jeremy R Hajek <hajek@iit.edu>
2021-05-20 22:41:13 -0500

    add provider template

---
+++ terraform/proxmox/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.7.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure = true
+  pm_api_url      = var.pm_api_url
+  pm_user         = var.pm_user
+  pm_password     = var.pm_password
+}
