9d90efd5d6a179a241ecbe252aa2a9df6df23ab9
Jeremy R Hajek <hajek@iit.edu>
2021-07-03 23:05:14 -0500

    update: removed username and password in place of API tokens

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -10,8 +10,6 @@ terraform {
 provider "proxmox" {
   pm_tls_insecure = true
   pm_api_url      = var.pm_api_url
-#  pm_user         = var.pm_user
-#  pm_password     = var.pm_password
   pm_api_token_id = var.pm_api_token_id
   pm_api_token_secret = var.pm_api_token_secret
   pm_log_enable = var.pm_log_enable
