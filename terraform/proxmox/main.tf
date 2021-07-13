92fbefc8233d7b4d1f2f88c9f879c138b99d6b80
Jeremy R Hajek <hajek@iit.edu>
2021-07-13 11:14:14 -0500

    update: cjamged consul_node to name as a single random ID

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -63,8 +63,9 @@ resource "consul_service" "proxmox" {
 }
 
 resource "consul_node" "compute" {
-  count   = var.numberofvms
-  name    = "${var.yourinitials}-vm${count.index}"
+#  count   = var.numberofvms
+  name    = "${random_id.id.dec}"
+#  name    = "${var.yourinitials}-vm${count.index}"
   address = ""
   
   connection {
