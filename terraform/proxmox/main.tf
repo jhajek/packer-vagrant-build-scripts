bea39416a8ee2304a5c072a318e99abdc4de9940
Jeremy R Hajek <hajek@iit.edu>
2021-07-13 11:04:20 -0500

    update: added count = var.numberofvms back to consul_service due to count.index requirement of declaration of count variable

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -49,6 +49,7 @@ resource "proxmox_vm_qemu" "test" {
 # https://registry.terraform.io/providers/hashicorp/consul/latest/docs/resources/service
 # How to add the consul_service to the terraform provider
 resource "consul_service" "proxmox" {
+  count   = var.numberofvms
   node    = "${consul_node.compute[count.index]}"
   
   connection {
