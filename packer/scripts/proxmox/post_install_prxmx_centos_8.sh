3ce2e122dcbf52217f7856cca6a2acc9f8356890
Jeremy R Hajek <hajek@iit.edu>
2021-07-17 23:25:55 -0500

    update: reworked setup script to not use ubuntu apt-get but use yum

--- packer/scripts/proxmox/post_install_prxmx_centos_8.sh
+++ packer/scripts/proxmox/post_install_prxmx_centos_8.sh
@@ -2,15 +2,7 @@
 set -e
 set -v
 
-# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
-# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
-# This line assumes the user you created in the preseed directory is vagrant
-echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
-sudo groupadd admin
-sudo usermod -a -G admin vagrant
-
-sudo apt-get update
-sudo apt-get install -y links firewalld
+sudo yum install -y git rsync wget vim curl
 
 sudo systemctl enable firewalld
 sudo systemctl start firewalld
