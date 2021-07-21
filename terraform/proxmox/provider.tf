2d2f0ea7b7c2dc500a84cbf817e1c73afccdbc8d
Jeremy R Hajek <hajek@iit.edu>
2021-07-20 22:39:00 -0500

    update: updated promox terraform provider to 2.7.4

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.7.1"
+      version = "2.7.4"
     }
     consul = {
       source = "hashicorp/consul"
