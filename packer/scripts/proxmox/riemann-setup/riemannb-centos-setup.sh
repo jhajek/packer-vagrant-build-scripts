9a163726d85cedc2b6664981486154ee3193063b
Jeremy R Hajek <hajek@iit.edu>
2021-08-05 14:24:43 -0500

    updates: changed sed to match my FQDNs

---
+++ packer/scripts/proxmox/riemann-setup/riemannb-centos-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+set -e
+set -v
+
+##################################################
+# Due to needing a tty to run sudo, this install command adds all the pre-reqs to build the virtualbox additions
+sudo yum install -y kernel-devel-`uname -r` gcc binutils make perl bzip2 vim wget git rsync
+###############################################################################################################
+# firewalld additions to make CentOS and riemann to work
+###############################################################################################################
+# Adding firewall rules for riemann - Centos 7 uses firewalld (Thanks Lennart...)
+# http://serverfault.com/questions/616435/centos-7-firewall-configuration
+# Websockets are TCP... for now - http://stackoverflow.com/questions/4657033/javascript-websockets-with-udp
+sudo firewall-cmd --permanent --zone=public --add-port=5555/tcp --permanent
+sudo firewall-cmd --permanent --zone=public --add-port=5556/udp --permanent
+sudo firewall-cmd --permanent --zone=public --add-port=5557/tcp --permanent
+sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp --permanent
+sudo firewall-cmd --permanent --zone=public --add-port=3000/tcp --permanent
+sudo firewall-cmd --reload
+###############################################################################################################
+# cloning source code examples for the book
+git clone https://github.com/turnbullpress/aom-code.git
+
+sudo cp -v /home/vagrant/aom-code/4/riemann/riemann.config /etc/riemann
+sudo cp -rv /home/vagrant/aom-code/4/riemann/examplecom /etc/riemann
+# Doing a find and replace for the stock riemannmc for the my FQDN
+sudo sed -i 's/riemannmc/jrh-riemannmc' /etc/riemann/riemann.config
+# Change the default name to match my FQDN
+sudo sed -i 's/graphitea/jrh-graphiteb' /etc/riemann/examplecom/etc/graphite.clj
+
+sudo systemctl daemon-reload
+sudo systemctl restart riemann
+
+# Install leiningen on Centos 8 - needed for riemann syntax checker
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod +x lein
+sudo cp ./lein /usr/local/bin
+
+# Riemann syntax checker download and install
+git clone https://github.com/samn/riemann-syntax-check
+cd riemann-syntax-check
+lein uberjar
+
+echo "All Done!"
---
+++ packer/scripts/proxmox/riemann-setup/riemannmc-ubuntu-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+set -e
+set -v
+
+# cloning source code examples for the book
+git clone https://github.com/turnbullpress/aom-code.git
+
+sudo mv -v /home/vagrant/aom-code/4/riemann/riemann.config_riemannmc /etc/riemann/riemann.config
+
+sudo sed -i 's/graphitea/jrh-graphitemc' /home/vagrant/aom-code/4/riemann/examplecom/etc/graphite.clj
+sudo sed -i 's/productiona/jrh-productionmc' /home/vagrant/aom-code/4/riemann/examplecom/etc/graphite.clj
+
+# Install leiningen on Ubuntu - needed for riemann syntax checker
+sudo apt-get install -y leiningen
+
+# Riemann syntax checker download and install
+git clone https://github.com/samn/riemann-syntax-check
+cd riemann-syntax-check
+lein uberjar
+cd ../
