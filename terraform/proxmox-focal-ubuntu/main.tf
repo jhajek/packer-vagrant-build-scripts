4c6c9c48846827cdb1aa6f6c10ec8d32904a0ab4
Jeremy R Hajek <hajek@iit.edu>
2021-08-11 12:17:59 -0500

    updated templates

--- terraform/proxmox-focal-ubuntu/main.tf
+++ terraform/proxmox-focal-ubuntu/main.tf
@@ -8,7 +8,7 @@ resource "random_id" "id" {
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/shuffle#example-usage
 resource "random_shuffle" "datadisk" {
-  input        = ["datadisk2", "datadisk3"]
+  input        = ["iscsi-1-1","iscsi-2-1","iscsi-3-1","iscsi-4-1","iscsi-5-1"]
   result_count = 1
 }
 
--- terraform/proxmox-riemann/main.tf
+++ terraform/proxmox-riemann/main.tf
@@ -10,7 +10,7 @@ resource "random_id" "id" {
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/shuffle#example-usage
 resource "random_shuffle" "datadisk" {
-  input = ["iscsi-1-1","iscsi-2-1","iscsi-3-1","iscsi-4-1","iscsi-5-1"]
+  input = ["datadisk2","datadisk3"]
   result_count = 1 
 }
 
