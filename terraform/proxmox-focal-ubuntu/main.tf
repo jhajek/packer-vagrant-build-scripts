c75ca7862c852dee833e6374fadbc549b66a0313
Jeremy R Hajek <hajek@iit.edu>
2021-08-11 11:21:19 -0500

    updated list of disks

--- terraform/proxmox-focal-ubuntu/main.tf
+++ terraform/proxmox-focal-ubuntu/main.tf
@@ -8,7 +8,7 @@ resource "random_id" "id" {
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/shuffle#example-usage
 resource "random_shuffle" "datadisk" {
-  input        = ["datadisk1", "datadisk2", "datadisk3", "datadisk4", "datadisk5"]
+  input        = ["datadisk2", "datadisk3"]
   result_count = 1
 }
 
