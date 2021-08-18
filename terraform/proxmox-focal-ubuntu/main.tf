11372dec6fb08825b3d4a2e5ad84dff88716b675
Jeremy R Hajek <hajek@iit.edu>
2021-08-17 20:46:36 -0500

    update: addded find and replace for consul FQDN

--- terraform/proxmox-focal-ubuntu/main.tf
+++ terraform/proxmox-focal-ubuntu/main.tf
@@ -56,6 +56,7 @@ resource "proxmox_vm_qemu" "vanilla-server" {
       "sudo sed -i 's/changeme/${random_id.id.dec}${count.index}/' /etc/consul.d/system.hcl",
       "sudo sed -i 's/replace-name/${var.yourinitials}-vm${count.index}/' /etc/consul.d/system.hcl",
       "sudo sed -i 's/ubuntu-server/${var.yourinitials}-vm${count.index}/' /etc/hosts",
+      "sudo sed -i 's/FQDN/${var.yourinitials}-vm${count.index}.service.consul/' /etc/update-motd.d/999-consul-dns-message",
       "sudo sed -i 's/#datacenter = \"my-dc-1\"/datacenter = \"rice-dc-1\"/' /etc/consul.d/consul.hcl",
       "echo 'retry_join = [\"${var.consulip}\"]' | sudo tee -a /etc/consul.d/consul.hcl",
       "sudo systemctl daemon-reload",
