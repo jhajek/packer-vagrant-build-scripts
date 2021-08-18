0682f99742df90221b174c59abeac13c5f46543c
Jeremy R Hajek <hajek@iit.edu>
2021-08-17 20:11:10 -0500

    update: changed motd message

--- packer/scripts/proxmox/focal-ubuntu/post_install_update_dynamic_motd_message.sh
+++ packer/scripts/proxmox/focal-ubuntu/post_install_update_dynamic_motd_message.sh
@@ -10,12 +10,12 @@ cat > /etc/update-motd.d/999-consul-dns-message <<'EOF'
 #!/bin/sh
 echo
 echo "############################################################"
-echo "# This is an ITM Cloud Lab Elastic Instance.               #"
-echo "# This cloud provided dynamic DNS resolution.              #"
-echo "# There is a private meta-network attached to each instance at: #"
+echo "# This is an ITM Cloud Lab Elastic Instance."
+echo "# This cloud provided dynamic DNS resolution."
+echo "# There is a private meta-network attached to each instance at: "
 echo "# 0.0.0.0                                                  #"
 echo "# Any instance in the cloud can be accessed by attaching the FQDN of *.service.consul #" 
-echo "# Your Fully Qualified Domain Name is:                     #"
+echo "# Your Fully Qualified Domain Name is: "
 EOF
 
 chmod a+x /etc/update-motd.d/999-dns-message
