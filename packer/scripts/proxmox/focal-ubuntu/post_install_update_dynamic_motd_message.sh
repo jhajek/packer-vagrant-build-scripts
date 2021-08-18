2a5b343ce63335179677a23d6f699880aed63943
Jeremy R Hajek <hajek@iit.edu>
2021-08-17 20:43:15 -0500

    update: fixed broken path

--- packer/scripts/proxmox/focal-ubuntu/post_install_update_dynamic_motd_message.sh
+++ packer/scripts/proxmox/focal-ubuntu/post_install_update_dynamic_motd_message.sh
@@ -6,16 +6,16 @@
 # https://ownyourbits.com/2017/04/05/customize-your-motd-login-message-in-debian-and-ubuntu/
 #############################################################################################
 
-cat > /etc/update-motd.d/999-consul-dns-message <<'EOF'
+sudo cat > /etc/update-motd.d/999-consul-dns-message <<'EOF'
 #!/bin/sh
 echo
 echo "############################################################"
-echo "# This is an ITM Cloud Lab Elastic Instance."
-echo "# This cloud provided dynamic DNS resolution."
-echo "# There is a private meta-network attached to each instance at: "
-echo "# 0.0.0.0                                                  #"
-echo "# Any instance in the cloud can be accessed by attaching the FQDN of *.service.consul #" 
-echo "# Your Fully Qualified Domain Name is: "
+echo "This is an ITM Cloud Lab Elastic Instance."
+echo "This cloud provided dynamic DNS resolution."
+echo "Any instance in the cloud can be accessed by attaching the FQDN of *.service.consul #" 
+echo "Your Fully Qualified Domain Name is: FQDN"
+echo "There is a private meta-network attached to each instance at: "
+echo "`hostname  -i | awk '{print $1}'`"
 EOF
 
-chmod a+x /etc/update-motd.d/999-dns-message
+sudo chmod a+x /etc/update-motd.d/999-consul-dns-message
